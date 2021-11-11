7a15b5151eff375edeb64300814b413f9def1900
vikashminj <vikashminj1999@gmail.com>
2021-11-11 15:24:03 +0530

    note serializer file name changing

--- NotesApp/serializers.py
+++ NotesApp/serializers.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from .models import NotesModel
 
 class NoteSerializer(serializers.ModelSerializer):
-    imagefield = serializers.ImageField()
+    image = serializers.ImageField(max_length=None, allow_empty_file=False, use_url=True)
     class Meta:
         model = NotesModel
-        fields = ['title','body','imagefield','user']
+        fields = ['title','body','image','user']
