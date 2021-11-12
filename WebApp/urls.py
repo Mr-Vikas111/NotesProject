e5f34f1e3a4c39ba6310b938baa90dc6aff0dbb2
vikashminj <vikashminj1999@gmail.com>
2021-11-12 16:38:37 +0530

    changes on webapp template and views

--- WebApp/urls.py
+++ WebApp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UserSignup,UserLogin,UserLogout
+from .views import UserSignup,UserLogin,UserLogout,UserProfile
 urlpatterns=[
     path('register/',UserSignup,name='regiteruser'),
     path('login/',UserLogin,name='loginuser'),
     path('logout/',UserLogout,name='logoutuser'),
+    path('profile/',UserProfile,name='profile')
 
     
 ]
--- WebApp/views.py
+++ WebApp/views.py
@@ -1,41 +1,14 @@
-from django.shortcuts import render, HttpResponseRedirect
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import AuthenticationForm
-from .forms import signupform, loginform
-from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
-
+from django.shortcuts import render, HttpResponseRedirect,redirect
 
 def UserSignup(request):
-    if request.method == "POST":
-        fm = signupform(request.POST)
-        if fm.is_valid():
-            fm.save()
-            messages.success(request, 'Account Created Successfully')
-
-    else:
-        fm = signupform()
-    return render(request, 'webapp/signup.html', {'form': fm})
+    return render(request, 'webapp/signup.html')
 
 
 def UserLogin(request):
-    if not request.user.is_authenticated:
-        if request.method == 'POST':
-            fm = loginform(request=request, data=request.POST)
-            if fm.is_valid():
-                uname = fm.cleaned_data['username']
-                upass = fm.cleaned_data['password']
-                user = authenticate(username=uname, password=upass)
-                if user is not None:
-                    login(request, user)
-                    messages.success(request, 'logged successful')
-                    return redirect('profile/')
-        else:
-            fm = loginform()
-        return render(request, 'webapp/userlogin.html', {'form': fm})
+    return render(request, 'webapp/userlogin.html')
 
 
-def profile(request):
+def UserProfile(request):
     return render(request, 'webapp/profile.html')
 
 
