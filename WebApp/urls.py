486e15683988fb0bfc9eba86af9e007580059ed1
vikashminj <vikashminj1999@gmail.com>
2021-11-13 10:28:00 +0530

    work on profile logout button

--- WebApp/urls.py
+++ WebApp/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import UserSignup,UserLogin,UserLogout,UserProfile
 urlpatterns=[
-    path('register/',UserSignup,name='regiteruser'),
-    path('login/',UserLogin,name='loginuser'),
-    path('logout/',UserLogout,name='logoutuser'),
+    path('register/',UserSignup,name='regiter'),
+    path('login/',UserLogin,name='login'),
+    path('logout/',UserLogout,name='logout'),
     path('profile/',UserProfile,name='profile')
 
     
